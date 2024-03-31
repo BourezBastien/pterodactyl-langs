caf98cd0cbbaf570bf8e084145fd055ab6df6cd2
bourez bastien <bourezbastien@gmail.com>
2024-03-31 17:59:15 +0200

    ✨ (index.blade.php): Add new view for displaying a list of users with their details and pagination functionality ✨ (new.blade.php): Add new view for creating a new user with form fields for user details and permissions, including password generation functionality
    📝 (view.blade.php): Add a new blade template for viewing user details in French language.

---
+++ French/resources/views/admin/users/new.blade.php
@@ -0,0 +1,128 @@
+@extends('layouts.admin')
+
+@section('title')
+    Créer un utilisateur
+@endsection
+
+@section('content-header')
+    <h1>Créer un utilisateur<small>Ajouter un nouvel utilisateur au système.</small></h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('admin.index') }}">Admin</a></li>
+        <li><a href="{{ route('admin.users') }}">Utilisateurs</a></li>
+        <li class="active">Créer</li>
+    </ol>
+@endsection
+
+@section('content')
+<div class="row">
+    <form method="post">
+        <div class="col-md-6">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Identité</h3>
+                </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="email" class="control-label">Email</label>
+                        <div>
+                            <input type="text" autocomplete="off" name="email" value="{{ old('email') }}" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="username" class="control-label">Nom d'utilisateur</label>
+                        <div>
+                            <input type="text" autocomplete="off" name="username" value="{{ old('username') }}" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="name_first" class="control-label">Prénom du client</label>
+                        <div>
+                            <input type="text" autocomplete="off" name="name_first" value="{{ old('name_first') }}" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="name_last" class="control-label">Nom de famille du client</label>
+                        <div>
+                            <input type="text" autocomplete="off" name="name_last" value="{{ old('name_last') }}" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Langue par défaut</label>
+                        <div>
+                            <select name="language" class="form-control">
+                                @foreach($languages as $key => $value)
+                                    <option value="{{ $key }}" @if(config('app.locale') === $key) selected @endif>{{ $value }}</option>
+                                @endforeach
+                            </select>
+                            <p class="text-muted"><small>La langue par défaut à utiliser lors de l'affichage du Panneau pour cet utilisateur.</small></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-footer">
+                    {!! csrf_field() !!}
+                    <input type="submit" value="Créer un utilisateur" class="btn btn-success btn-sm">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Permissions</h3>
+                </div>
+                <div class="box-body">
+                    <div class="form-group col-md-12">
+                        <label for="root_admin" class="control-label">Administrateur</label>
+                        <div>
+                            <select name="root_admin" class="form-control">
+                                <option value="0">@lang('strings.no')</option>
+                                <option value="1">@lang('strings.yes')</option>
+                            </select>
+                            <p class="text-muted"><small>En définissant cette option sur 'Oui', un utilisateur aura un accès administratif complet.</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Mot de passe</h3>
+                </div>
+                <div class="box-body">
+                    <div class="alert alert-info">
+                        <p>Fournir un mot de passe utilisateur est facultatif. Les nouveaux utilisateurs reçoivent un email pour créer un mot de passe la première fois qu'ils se connectent. Si un mot de passe est fourni ici, vous devrez trouver une méthode différente pour le fournir à l'utilisateur.</p>
+                    </div>
+                    <div id="gen_pass" class=" alert alert-success" style="display:none;margin-bottom: 10px;"></div>
+                    <div class="form-group">
+                        <label for="pass" class="control-label">Mot de passe</label>
+                        <div>
+                            <input type="password" name="password" class="form-control" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
+
+@section('footer-scripts')
+    @parent
+    <script>$("#gen_pass_bttn").click(function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: "GET",
+                url: "/password-gen/12",
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+               },
+                success: function(data) {
+                    $("#gen_pass").html('<strong>Mot de passe généré :</strong> ' + data).slideDown();
+                    $('input[name="password"], input[name="password_confirmation"]').val(data);
+                    return false;
+                }
+            });
+            return false;
+        });
+    </script>
+@endsection
---
+++ French/resources/views/admin/users/view.blade.php
@@ -0,0 +1,123 @@
+@extends('layouts.admin')
+
+@section('title')
+    Gérer l'utilisateur : {{ $user->username }}
+@endsection
+
+@section('content-header')
+    <h1>{{ $user->name_first }} {{ $user->name_last}}<small>{{ $user->username }}</small></h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('admin.index') }}">Admin</a></li>
+        <li><a href="{{ route('admin.users') }}">Utilisateurs</a></li>
+        <li class="active">{{ $user->username }}</li>
+    </ol>
+@endsection
+
+@section('content')
+<div class="row">
+    <form action="{{ route('admin.users.view', $user->id) }}" method="post">
+        <div class="col-md-6">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Identité</h3>
+                </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="email" class="control-label">Email</label>
+                        <div>
+                            <input type="email" name="email" value="{{ $user->email }}" class="form-control form-autocomplete-stop">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="registered" class="control-label">Nom d'utilisateur</label>
+                        <div>
+                            <input type="text" name="username" value="{{ $user->username }}" class="form-control form-autocomplete-stop">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="registered" class="control-label">Prénom du client</label>
+                        <div>
+                            <input type="text" name="name_first" value="{{ $user->name_first }}" class="form-control form-autocomplete-stop">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="registered" class="control-label">Nom de famille du client</label>
+                        <div>
+                            <input type="text" name="name_last" value="{{ $user->name_last }}" class="form-control form-autocomplete-stop">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Langue par défaut</label>
+                        <div>
+                            <select name="language" class="form-control">
+                                @foreach($languages as $key => $value)
+                                    <option value="{{ $key }}" @if($user->language === $key) selected @endif>{{ $value }}</option>
+                                @endforeach
+                            </select>
+                            <p class="text-muted"><small>La langue par défaut à utiliser lors de l'affichage du Panneau pour cet utilisateur.</small></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-footer">
+                    {!! csrf_field() !!}
+                    {!! method_field('PATCH') !!}
+                    <input type="submit" value="Mettre à jour l'utilisateur" class="btn btn-primary btn-sm">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Mot de passe</h3>
+                </div>
+                <div class="box-body">
+                    <div class="alert alert-success" style="display:none;margin-bottom:10px;" id="gen_pass"></div>
+                    <div class="form-group no-margin-bottom">
+                        <label for="password" class="control-label">Mot de passe <span class="field-optional"></span></label>
+                        <div>
+                            <input type="password" id="password" name="password" class="form-control form-autocomplete-stop">
+                            <p class="text-muted small">Laissez vide pour conserver le mot de passe de cet utilisateur. L'utilisateur ne recevra aucune notification en cas de changement de mot de passe.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Permissions</h3>
+                </div>
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="root_admin" class="control-label">Administrateur</label>
+                        <div>
+                            <select name="root_admin" class="form-control">
+                                <option value="0">@lang('strings.no')</option>
+                                <option value="1" {{ $user->root_admin ? 'selected="selected"' : '' }}>@lang('strings.yes')</option>
+                            </select>
+                            <p class="text-muted"><small>En définissant cette option sur 'Oui', un utilisateur aura un accès administratif complet.</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <div class="col-xs-12">
+        <div class="box box-danger">
+            <div class="box-header with-border">
+                <h3 class="box-title">Supprimer l'utilisateur</h3>
+            </div>
+            <div class="box-body">
+                <p class="no-margin">Il ne doit y avoir aucun serveur associé à ce compte pour qu'il soit supprimé.</p>
+            </div>
+            <div class="box-footer">
+                <form action="{{ route('admin.users.view', $user->id) }}" method="POST">
+                    {!! csrf_field() !!}
+                    {!! method_field('DELETE') !!}
+                    <input id="delete" type="submit" class="btn btn-sm btn-danger pull-right" {{ $user->servers->count() < 1 ?: 'disabled' }} value="Supprimer l'utilisateur" />
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
